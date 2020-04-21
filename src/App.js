dcbd1e41c7b5c8658d69b26fa55ded703ce75611
marby <marbysneaker@gmail.com>
2020-04-20 23:11:23 -0700

    new commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+class App extends Component{
+
+
+state = {
+  list: [
+  {text: "Buy eggs", done: false},
+  {text: "Buy milk", done: true},
+  ]
+  }
+
+render() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className = "todo-list">
+          <ul>
+          {this.state.list.map((todo,index)=>
+              <li key={index}>{todo.text}</li>
+            
+            )}
+          
+          </ul>
+        </div>
       </header>
     </div>
   );
 }
-
+}
 export default App;
