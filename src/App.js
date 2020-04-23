15f19a2422ee02c13d6e528f8a56e574a3c62abc
marby <marbysneaker@gmail.com>
2020-04-23 00:33:51 -0700

    ready for design

--- src/App.js
+++ src/App.js
@@ -20,8 +20,8 @@ class App extends Component{
 
 
 state = {
-  list: [{text: "Buy eggs", notes:'empty',done: false},
-  {text: "Buy milk", notes:'empty',done: true},
+  list: [{text: "Buy eggs", notes:'empty',done: false,toggle:false},
+  {text: "Buy milk", notes:'empty',done: true,toggle:false},
   ],
   input: '',
   textInput:'',
@@ -30,7 +30,8 @@ state = {
   modalContent:[],
   modalIndex:null,
   modalInput:"",
-  modalTextInput:''
+  modalTextInput:'',
+  toggle:false
   }
 onInputChange = (event) => {
 
@@ -47,15 +48,16 @@ onModalTextInputChange = (event) => {
   this.setState({modalTextInput:event.target.value})
 }
 onClicked = () => {
-  console.log(this.state.input)
-  this.setState(prevState => ({
-    list: [...prevState.list, {text: this.state.input,notes:this.state.textInput,done:false}]
-  }))
-  // this.state.list.push({text:this.state.input, done: false})
-  console.log(this.state.list)
-  this.setState({input:''})
-  this.setState({textInput:''})
-
+  if(this.state.input){
+      console.log(this.state.input)
+      this.setState(prevState => ({
+        list: [...prevState.list, {text: this.state.input,notes:this.state.textInput,done:false,toggle:false}]
+      }))
+      // this.state.list.push({text:this.state.input, done: false})
+      console.log(this.state.list)
+      this.setState({input:''})
+      this.setState({textInput:''})
+  }
 }
 // componentDidMount = () => {
 //   let newData = this.state.list
@@ -91,15 +93,38 @@ showModal = index => {
   this.setState({modal:true})
   let modalList = (this.state.list[index])
   this.state.modalContent = modalList
-  console.log(this.state.modalContent)
-  console.log(this.state.modalContent.text)
   this.setState({modalIndex:index})
   this.setState({modalContent : modalList})
+  this.setState({modalInput : this.state.modalContent.text})
+  this.setState({modalTextInput:this.state.modalContent.notes})
+  console.log(this.state.modalInput)
 }
 onEdit = (index) => {
-  let list = this.state.list
-  list.splice(index, 1 , {text:this.state.modalInput,notes:this.state.modalTextInput,done:false})
+  if(this.state.modalInput){
+      let list = this.state.list
+      list.splice(index, 1 , {text:this.state.modalInput,notes:this.state.modalTextInput,done:false})
+      this.setState({list})
+      console.log(index)
+      this.setState({modal:false})
+
+  }
 }
+  onToggle = (index) => {
+    console.log(this.state.list[index])
+    let list = this.state.list
+    let toggle = list[index]
+    if(toggle.toggle === false){
+      toggle.toggle = true
+      
+    }
+    else{
+      toggle.toggle = false
+      
+    }
+    console.log(toggle.toggle)
+    this.setState({list})
+    console.log(this.state.list)
+  }
 
 render() {
   return (
@@ -110,7 +135,13 @@ render() {
           {(this.state.list != 0)? (this.state.list.map((todo,index)=>
               <div key={index} ><span onClick={()=> this.showModal(index)}>{todo.text} </span>
               <button onClick={() => this.changeToDone(index)}>{todo.done?<Emoji label="sheep" symbol="✅"/>:'❌'}</button>
-              <button onClick={()=> this.onClicked(index)}>Delete</button></div>
+              <button className='delete' onClick={()=> this.deleteItem(index)}>Delete</button>
+              <button onClick={()=> this.onToggle(index)}>toggle</button>
+              
+             <br></br>
+              <div id="toggle" className={(todo.toggle)?("toggle-true"):('toggle-false')}>{(todo.toggle)?(<span>{this.state.list[index].notes}<span><br></br></span></span>):('')}</div>
+              
+              </div>
               )):(<div>Nothing to do</div>)}
           <div className='input'>
               <input type='text' className='todo-item' value={this.state.input} onChange={this.onInputChange}></input><br></br><br></br>
@@ -124,10 +155,14 @@ render() {
            <div className='modal'>
               <br></br>
               <br></br>
-              <h1>{this.state.modalContent.text}</h1>
-              <textarea placeholder='add notes' name="message" rows="10" cols="30" value={this.state.modalContent.notes} onChange={this.onModalTextInputChange}>{this.state.modalContent.notes}</textarea>
+              <input type='text' className='modal-tex' value={this.state.modalInput} onChange={this.onModalInputChange}></input>
+
               <br></br>
               <br></br>
+
+              <textarea placeholder='add notes' name="message" rows="10" cols="30" onChange={this.onModalTextInputChange} value={this.state.modalTextInput}>{this.state.modalTextInput}</textarea>
+              <br></br>
+              <button onClick={()=> this.onEdit(this.state.modalIndex)}>Edit</button>
               
            
            </div>
